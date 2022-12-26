ec2cb2a739f7a9325cb43d75c2b94cafff0e8377
Enes Büyük <contact@enesbuyuk.com>
2022-12-26 15:52:54 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-func GetIpAddress(w http.ResponseWriter, r *http.Request) {
+func GetIPAddress(w http.ResponseWriter, r *http.Request) {
     ip := r.RemoteAddr
     xforward := r.Header.Get("X-Forwarded-For")
     fmt.Println("Url:"+r.URL.Path+"<->IP : "+ip+" <-> X-Forwarded-For : "+ xforward)
